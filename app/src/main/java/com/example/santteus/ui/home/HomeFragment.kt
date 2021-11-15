3c1f6f3cb80e1e47fa0cfb97cc6d85c819d83ec5
serim53 <tpfla9953@naver.com>
2021-11-15 17:25:47 +0900

    DetailBottomSheet/RecordFragment

--- app/src/main/java/com/example/santteus/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/santteus/ui/home/HomeFragment.kt
@@ -8,16 +8,19 @@ import android.hardware.SensorManager
 import android.os.Bundle
 
 import android.os.SystemClock
+import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.example.santteus.databinding.FragmentHomeBinding
 import com.example.santteus.ui.run.RunFinishFragment
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -26,12 +29,16 @@ import java.util.*
 import com.google.firebase.database.*
 
 
-class HomeFragment : Fragment(), OnMapReadyCallback, SensorEventListener {
+class HomeFragment : Fragment(), OnMapReadyCallback, SensorEventListener, GoogleMap.OnMarkerClickListener {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding ?: error("Binding이 초기화되지 않았습니다.")
 
     private lateinit var mView: MapView
+
+    // 전달용 변수
+    private var roadName = ""
+    private var roadPosition = LatLng(37.568291, 126.997780)
 
     private var time = 0
     private var isRunning = false
@@ -41,6 +48,7 @@ class HomeFragment : Fragment(), OnMapReadyCallback, SensorEventListener {
     private var mStepsCount = 0
 
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+    private lateinit var detailBottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
     private lateinit var sensorManager :SensorManager
 
     override fun onCreateView(
@@ -56,6 +64,7 @@ class HomeFragment : Fragment(), OnMapReadyCallback, SensorEventListener {
         setListeners()
         setBottomSheet()
         setSensorCount()
+        setDetailBottomSheet()
 
         return binding.root
     }
@@ -73,6 +82,12 @@ class HomeFragment : Fragment(), OnMapReadyCallback, SensorEventListener {
     }
 
     private fun setListeners() {
+
+        //운동 시작버튼 클릭이벤트
+        binding.detailBottom.btnRunStart.setOnClickListener {
+
+        }
+
         binding.btnRun.setOnClickListener {
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
             bottomSheetBehavior.isDraggable = false
@@ -175,6 +190,7 @@ class HomeFragment : Fragment(), OnMapReadyCallback, SensorEventListener {
             }
         })
 
+        googleMap.setOnMarkerClickListener(this)
 
         //val marker = LatLng(37.568291,126.997780)
         //googleMap.addMarker(MarkerOptions().position(marker).title("여기"))
@@ -237,6 +253,109 @@ class HomeFragment : Fragment(), OnMapReadyCallback, SensorEventListener {
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
 
+    }
+
+    override fun onMarkerClick(p0: Marker): Boolean {
+
+        // 마커 클릭 후 변수에 위치 이름, 경/위도 저장
+        roadName = p0.title.toString()
+        roadPosition = p0.position
+
+        binding.detailBottom.explain.setMovementMethod(ScrollingMovementMethod());
+
+        var distanceLong : Long
+        var distanceDouble : Double
+        var time : String
+        var toilet : String
+        var conven : String
+
+        val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+        val myRef : DatabaseReference = database.getReference("road")
+
+        myRef.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                for (snapshot in dataSnapshot.children) {
+                    if(snapshot.child("WLK_COURS_NM").value == roadName){
+
+                        time = snapshot.child("COURS_TIME_CN").value as String
+                        if (snapshot.child("COURS_DETAIL_LT_CN").value is Long){
+                            distanceLong = snapshot.child("COURS_DETAIL_LT_CN").value as Long
+                            binding.detailBottom.distanceTime.setText(distanceLong.toString() + "km | " + time + "분")
+                        }else if(snapshot.child("COURS_DETAIL_LT_CN").value is Double){
+                            distanceDouble = snapshot.child("COURS_DETAIL_LT_CN").value as Double
+                            binding.detailBottom.distanceTime.setText(distanceDouble.toString() + "km | " + time + "분")
+                        }
+
+                        toilet = snapshot.child("TOILET_DC").value as String
+                        conven = snapshot.child("CVNTL_NM").value as String
+
+                        binding.detailBottom.roadName.setText(snapshot.child("WLK_COURS_NM").value as String)
+                        binding.detailBottom.roadLevel.setText(snapshot.child("COURS_LEVEL_NM").value as String)
+
+
+                        if(toilet!="" && toilet!="없음"){
+                            if(conven!="" && conven!="없음"){
+                                binding.detailBottom.toiletConve.setText("화장실 | 편의점")
+                            }else{
+                                binding.detailBottom.toiletConve.setText("화장실 | 편의점없음")
+                            }
+                        }else{
+                            if(conven!="" && conven!="없음"){
+                                binding.detailBottom.toiletConve.setText("화장실없음 | 편의점")
+                            }else{
+                                binding.detailBottom.toiletConve.setText("화장실없음 | 편의점없음")
+                            }
+                        }
+
+                        binding.detailBottom.route.setText(snapshot.child("COURS_DC").value as String)
+                        binding.detailBottom.explain.setText(snapshot.child("ADIT_DC").value as String)
+
+                    }
+
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                //Log.e("MainActivity", String.valueOf(databaseError.toException())); // 에러문 출력
+            }
+        })
+
+
+        detailBottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        detailBottomSheetBehavior.isDraggable = true
+
+        return true
+
+    }
+
+
+
+    private fun setDetailBottomSheet() {
+        detailBottomSheetBehavior = BottomSheetBehavior.from(binding.detailBottom.bottomView)
+        detailBottomSheetBehavior.isGestureInsetBottomIgnored = true
+        detailBottomSheetBehavior.apply {
+            state = BottomSheetBehavior.STATE_HIDDEN
+            isDraggable = true
+        }
+        detailBottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_DRAGGING) {
+                    //detailBottomSheetBehavior.peekHeight = 430
+//                    if (newState != BottomSheetBehavior.STATE_EXPANDED){
+//                        detailBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED)
+//                        detailBottomSheetBehavior.peekHeight = 430
+//                    }
+
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+            }
+        })
     }
 
 }
